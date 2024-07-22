d81681877c75f4a76ece15303716f870eccb1bc8
vespora <vieonix@proton.me>
2024-07-22 19:38:20 -0400

    base path configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 function Config() {
   return defineConfig({
+    base: "/RESTAR-CodingExercise/",
     plugins: [react(), dsv()],
     build: {
       rollupOptions: {
